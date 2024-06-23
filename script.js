5063654a34b0c30b47f6cac7bce65ec5ef6bbb69
NunzioBasciano <nbasho1988@gmail.com>
2024-06-23 19:45:22 +0200

    final commit

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 import { cardGen, GET, getWithCoordinates, capitalize } from "./modules/components.js";
-
 import { provinces, marineLocations, backgrounds, backgroundsSea } from "./modules/data.js";
 
 // Seleziona il contenitore principale delle card delle province
@@ -10,6 +9,9 @@ const containerSeaEl = document.querySelector('.container-sea');
 
 // Seleziona l'elemento di selezione delle province nel DOM
 const selectElement = document.getElementById('provinces');
+
+
+const favoritesContainerEl = document.querySelector('.favorites-list-container');
 
 const searchFormEl = document.querySelector('#search-location');
 const searchInputEl = document.querySelector('.search-input');
@@ -74,7 +76,9 @@ const generateSelectedCard = async (selectedProvince) => {
         if (!selectedProvince || selectedProvince === 'seleziona una provincia siciliana') {
             await generateCards();  // Carica tutte le card delle province
 
+
         } else {
+
             // Ottieni i dati della provincia selezionata
             const data = await GET(selectedProvince);
 
@@ -94,7 +98,6 @@ const generateSelectedCard = async (selectedProvince) => {
 
 const generateSearchCard = async (cityName) => {
     try {
-
         const data = await GET(cityName);
         const card = cardGen(data, defaultBackgroundImage, cityName);
         containerEl.append(card);
@@ -109,20 +112,21 @@ selectElement.addEventListener('change', async function () {
 
     const selectedProvince = selectElement.value;  // Ottiene il valore selezionato
     generateSelectedCard(selectedProvince); // Chiama la funzione per generare la card della provincia selezionata
+    favoritesContainerEl.classList.add('display-none');  // Mostra il titolo dei 
 });
 
+// Event listener per il submit del form di ricerca
 searchFormEl.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    const cityName = searchInputEl.value.trim();
+    event.preventDefault();// Previene il comportamento di default del form
+    const cityName = searchInputEl.value.trim();// Ottiene il valore dell'input di ricerca
 
-    if (cityName) {
-        containerEl.textContent = '';
-        await generateSearchCard(cityName);
+    if (cityName) { // Se c'è un valore
+        containerEl.textContent = ''; // Svuota il contenitore delle card
+        await generateSearchCard(cityName); // Genera la card della città cercata
     }
 })
 
 // Inizializzazione: genera tutte le card delle province e delle località marine
-/* generateCards(); */
 generateCardsSea();
 
 const addFavoritesBtnEl = document.querySelector('.add-favorites');
@@ -130,41 +134,45 @@ const removeFavoritesBtnEl = document.querySelector('.remove-favorites');
 const inputFavoriteEl = document.querySelector('#favourites-input');
 let favoriteStorage = JSON.parse(localStorage.getItem('favorites')) || [];
 
+// Event listener per aggiungere ai preferiti
 addFavoritesBtnEl.addEventListener('click', async (event) => {
-    event.stopPropagation();
-    const favoriteEL = capitalize(inputFavoriteEl.value.trim());
-    if (favoriteEL && !favoriteStorage.includes(favoriteEL)) {
+    event.stopPropagation(); // Previene la propagazione dell'evento
+    const favoriteEL = capitalize(inputFavoriteEl.value.trim()); // Capitalizza il valore dell'input dei preferiti
+    if (favoriteEL && !favoriteStorage.includes(favoriteEL)) { // Se il valore è valido e non è già nei preferiti
         favoriteStorage.push(favoriteEL);
-        localStorage.setItem('favorites', JSON.stringify(favoriteStorage));
-        containerEl.innerHTML = '';
+        localStorage.setItem('favorites', JSON.stringify(favoriteStorage)); // Aggiorna il localStorage
+        containerEl.innerHTML = ''; // Svuota il contenitore delle card
         /*  for (let i = 0; i < favoriteStorage.length; i++) {
              generateSearchCard(favoriteStorage[i]);
          } */
-        favoriteStorage.map(el => generateSearchCard(el));
+        favoriteStorage.map(el => generateSearchCard(el));  // Genera le card per ciascun preferito
     }
 });
 
 
+// Se ci sono preferiti, genera le card corrispondenti
 if (favoriteStorage.length > 0) {
-    containerEl.innerHTML = '';
+    containerEl.innerHTML = '';  // Svuota il contenitore delle card
     for (let i = 0; i < favoriteStorage.length; i++) {
-        generateSearchCard(favoriteStorage[i]);
+        generateSearchCard(favoriteStorage[i]); // Genera la card per ciascun preferito
     }
-} else {
+
+} else { // Se non ci sono preferiti, nasconde il contenitore dei preferiti
     const favoritesContainerEl = document.querySelector('.favorites-list-container');
-    favoritesContainerEl.classList.add('display');
-    generateCards();
+    favoritesContainerEl.classList.add('display-none');
+    generateCards(); // Genera tutte le card delle province
 }
 
 
+// Event listener per rimuovere dai preferiti
 removeFavoritesBtnEl.addEventListener('click', async (event) => {
-    const deleteEl = capitalize(inputFavoriteEl.value.trim());
-    if (favoriteStorage.includes(deleteEl)) {
-        const indexToDelete = favoriteStorage.indexOf(deleteEl);
-        favoriteStorage.splice(indexToDelete, 1);
+    const deleteEl = capitalize(inputFavoriteEl.value.trim()); // Capitalizza il valore dell'input dei preferiti
+    if (favoriteStorage.includes(deleteEl)) { // Se il valore è nei preferiti
+        const indexToDelete = favoriteStorage.indexOf(deleteEl);  // Ottiene l'indice del valore da rimuovere
+        favoriteStorage.splice(indexToDelete, 1); // Rimuove il valore dai preferiti
 
         // aggiorna localstorage
         localStorage.setItem('favorites', JSON.stringify(favoriteStorage));
     }
-    window.location.reload();
+    window.location.reload(); // Ricarica la pagina
 });
